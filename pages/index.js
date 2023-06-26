5befeeda5c62603da09afe559deb56e55c78e2e2
fidaatag <fidaa2fidaa@gmail.com>
2023-06-26 10:46:50 +0700

    pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import styles from '../styles/Home.module.css';
 import Link from 'next/link';;
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <div className={styles.container}>
@@ -17,6 +27,21 @@ export default function Home() {
           <p>hello, I'm Fidaa. Now i'm a communications student and i want to be software enginer who can remote work </p>
           <Link href="/posts/first-post">first post</Link>
         </section>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
       </div>
     </Layout>
   )
